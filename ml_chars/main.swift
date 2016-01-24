91848f5f7b5d06c5d9d2b1056cc3cc300cd35ba7
Ryan Bernstein <lbryanlb@comcast.net>
2016-01-23 22:33:23 -0800

    Initial Commit

---
+++ ml_chars/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ml_chars
+//
+//  Created by Ryan Bernstein on 1/23/16.
+//  Copyright © 2016 Ryan Bernstein. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
